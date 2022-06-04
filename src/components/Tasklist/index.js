2f3c13eab5ac77a9af84c330ff4913e3a1500169
felipersan <feliperodrigues.san@gmail.com>
2022-06-04 11:33:57 -0300

    modificação do flatlist

--- src/components/Tasklist/index.js
+++ src/components/Tasklist/index.js
@@ -9,43 +9,28 @@ import {
 import Feather from 'react-native-vector-icons/Feather';
 
 export default function TaskList({tarefa, deleteItem, editItem}) {
-  const [altura, setAltura] = useState(45);
-
-  function slideOut() {
-    if (altura == 45) {
-      setAltura(80);
-    } else {
-      setAltura(45);
-    }
-  }
-
   return (
-    <View style={[styles.container, {height: altura}]}>
+    <View style={styles.container}>
       <View>
         <Text style={styles.tarefa}>{tarefa.nome}</Text>
-        <View style={styles.editarea}>
-          <TouchableOpacity onPress={() => editItem(tarefa)}>
-            <Text style={[styles.editTasks]}>
-              {altura !== 45 && 'Editar Tarefa'}
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.trash}
-            onPress={() => deleteItem(tarefa.key)}>
-            {altura !== 45 && (
-              <Feather name={'trash'} color="#121212" size={16} />
-            )}
-          </TouchableOpacity>
-        </View>
+        <Text style={styles.priority}>High</Text>
       </View>
+      <View style={styles.btnarea}>
+        <TouchableOpacity
+          style={{marginRight: 12}}
+          onPress={() => {
+            editItem(tarefa);
+          }}>
+          <Feather name="edit-3" color="#f1c617" size={21} />
+        </TouchableOpacity>
 
-      <TouchableOpacity onPress={slideOut}>
-        <Feather
-          name={altura == 45 ? 'chevron-down' : 'chevron-up'}
-          color="#121212"
-          size={20}
-        />
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            deleteItem(tarefa.key);
+          }}>
+          <Feather name={'trash'} color="#f1c617" size={21} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -53,16 +38,19 @@ export default function TaskList({tarefa, deleteItem, editItem}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0c517',
-    elevation: 1,
+    backgroundColor: '#fff',
+    borderRadius: 11,
+    elevation: 2,
+    height: 85,
     marginBottom: 5,
     padding: 10,
     marginHorizontal: 5,
     justifyContent: 'space-between',
+    alignItems: 'center',
     flexDirection: 'row',
   },
   tarefa: {
-    fontSize: 15,
+    fontSize: 19,
     color: '#121212',
   },
   editarea: {
@@ -74,7 +62,15 @@ const styles = StyleSheet.create({
     padding: 5,
     borderRadius: 4,
   },
-  trash: {
-    marginLeft: 10,
+  priority: {
+    backgroundColor: '#f1c617',
+    paddingHorizontal: 7,
+    paddingVertical: 4,
+    maxWidth: 45,
+    borderRadius: 7,
+    marginTop: 9,
+  },
+  btnarea: {
+    flexDirection: 'row',
   },
 });
